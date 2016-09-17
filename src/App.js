00f30c1103b6de3d061645dd78a45569a9b6d89c
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-09-17 14:35:21 +0200

    Design navigation

--- src/App.js
+++ src/App.js
@@ -8,11 +8,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <nav className="App-navigation">
+          <a className="App-navigation-logo-active" href="#">
+            <img src={logo} alt="Remote Stories" />
+          </a>
+
+          <ul className="App-navigation-list">
+            <li>
+              <a className="App-navigation-list-link-active" href="#">About</a>
+            </li>
+            <li>
+              <a className="App-navigation-list-link" href="#">New</a>
+            </li>
+            <li>
+              <a className="App-navigation-list-link" href="#">Popular</a>
+            </li>
+            <li>
+              <a className="App-navigation-list-link" href="#">Submit</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
