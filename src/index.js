106cf29b04a3917f7e209eee1123ba0435860aff
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-09-17 12:59:20 +0200

    Set up default font and sanitize the css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 /* @flow */
 
+import './index.css';
+import 'sanitize.css/sanitize.css';
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <App />,
