8a1120f272cc6543976e829f46856157973d89fc
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-09-18 19:08:36 +0200

    Make layout responsive

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 /* @flow */
 
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Stories from './stories';
 import logo from './logo.svg';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <nav className="App-navigation">
-          <a href="#">
+export default function App() {
+  return (
+    <div className="App">
+      <div className="App-illustration" />
+
+      <div className="App-content">
+        <nav className="App-content-navigation">
+          <a className="App-content-navigation-logo" href="#">
             <img src={logo} alt="Remote Stories" />
           </a>
 
-          <ul className="App-navigation-list">
+          <ul className="App-content-navigation-list">
             <li>
-              <a className="App-navigation-list-link-active" href="#">About</a>
+              <a className="App-content-navigation-list-link-active" href="#">About</a>
             </li>
             <li>
-              <a className="App-navigation-list-link" href="#">New</a>
+              <a className="App-content-navigation-list-link" href="#">New</a>
             </li>
             <li>
-              <a className="App-navigation-list-link" href="#">Popular</a>
+              <a className="App-content-navigation-list-link" href="#">Popular</a>
             </li>
             <li>
-              <a className="App-navigation-list-link" href="#">Submit</a>
+              <a className="App-content-navigation-list-link" href="#">Submit</a>
             </li>
           </ul>
         </nav>
 
         <Stories />
       </div>
-    );
-  }
+    </div>
+  );
 }
