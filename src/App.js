615dfd6aec4476caeb31ec66486ff95a7a487e07
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-09-18 20:44:54 +0200

    Set up react-router for routed pages

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
 /* @flow */
 
 import './App.css';
+import About from './about';
+import New from './new';
+import Popular from './popular';
 import React from 'react';
 import Stories from './stories';
+import Submit from './submit';
 import logo from './logo.svg';
+import { BrowserRouter, Match, Link } from 'react-router'
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="App-illustration" />
+    <BrowserRouter>
+      <div className="App">
+        <div className="App-illustration" />
 
-      <div className="App-content">
-        <nav className="App-content-navigation">
-          <a className="App-content-navigation-logo" href="#">
-            <img src={logo} alt="Remote Stories" />
-          </a>
+        <div className="App-content">
+          <nav className="App-content-navigation">
+            <Link className="App-content-navigation-logo" to="/">
+              <img src={logo} alt="Remote Stories" />
+            </Link>
 
-          <ul className="App-content-navigation-list">
-            <li>
-              <a className="App-content-navigation-list-link-active" href="#">About</a>
-            </li>
-            <li>
-              <a className="App-content-navigation-list-link" href="#">New</a>
-            </li>
-            <li>
-              <a className="App-content-navigation-list-link" href="#">Popular</a>
-            </li>
-            <li>
-              <a className="App-content-navigation-list-link" href="#">Submit</a>
-            </li>
-          </ul>
-        </nav>
+            <ul className="App-content-navigation-list">
+              <li>
+                <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/new">
+                  New
+                </Link>
+              </li>
+              <li>
+                <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/popular">
+                  Popular
+                </Link>
+              </li>
+              <li>
+                <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/submit">
+                  Submit
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Stories />
+          <Match exactly pattern="/" component={Stories} />
+          <Match pattern="/about" component={About} />
+          <Match pattern="/new" component={New} />
+          <Match pattern="/popular" component={Popular} />
+          <Match pattern="/submit" component={Submit} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
