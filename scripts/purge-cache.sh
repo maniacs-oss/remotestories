19f77ef5bdb44ce671330c77e7b2ca5c99e52fa6
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-09-18 22:27:25 +0200

    Purge cache after deploy

---
+++ scripts/purge-cache.sh
@@ -0,0 +1,5 @@
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
+-H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+-H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+-H "Content-Type: application/json" \
+--data '{"purge_everything":true}'
---
+++ scripts/zone-id.sh
@@ -0,0 +1,4 @@
+curl -X GET "https://api.cloudflare.com/client/v4/zones?name=remotestories.com&status=active&page=1&per_page=20&order=status&direction=desc&match=all" \
+-H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+-H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+-H "Content-Type: application/json"
