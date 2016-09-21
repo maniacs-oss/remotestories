d96d4fbfddb5652b4cca9891761a74722463be47
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-09-21 18:03:44 +0200

    Add daily illustration

--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,8 @@ import Popular from '../pages/popular';
 import React from 'react';
 import Stories from '../pages/stories';
 import Submit from '../pages/submit';
+import illustration1 from './illustration-1.jpg';
+import illustration2 from './illustration-2.jpg';
 import logo from './logo.svg';
 import { BrowserRouter, Match, Link } from 'react-router'
 
@@ -14,45 +16,53 @@ export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <div className="App-illustration" />
+        <div className="App-illustration">
+          <img className="App-illustration-image" src={illustrationOfTheDay()} alt="" />
+        </div>
 
-        <div className="App-content">
-          <nav className="App-content-navigation">
-            <Link className="App-content-navigation-logo" to="/">
-              <img src={logo} alt="Remote Stories" />
-            </Link>
+        <div className="App-container">
+          <div className="App-content">
+            <nav className="App-content-navigation">
+              <Link className="App-content-navigation-logo" to="/">
+                <img src={logo} alt="Remote Stories" />
+              </Link>
 
-            <ul className="App-content-navigation-list">
-              <li>
-                <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/new">
-                  New
-                </Link>
-              </li>
-              <li>
-                <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/popular">
-                  Popular
-                </Link>
-              </li>
-              <li>
-                <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/submit">
-                  Submit
-                </Link>
-              </li>
-            </ul>
-          </nav>
+              <ul className="App-content-navigation-list">
+                <li>
+                  <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/new">
+                    New
+                  </Link>
+                </li>
+                <li>
+                  <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/popular">
+                    Popular
+                  </Link>
+                </li>
+                <li>
+                  <Link activeClassName="App-content-navigation-list-link-active" className="App-content-navigation-list-link" to="/submit">
+                    Submit
+                  </Link>
+                </li>
+              </ul>
+            </nav>
 
-          <Match exactly pattern="/" component={Stories} />
-          <Match pattern="/about" component={About} />
-          <Match pattern="/new" component={New} />
-          <Match pattern="/popular" component={Popular} />
-          <Match pattern="/submit" component={Submit} />
+            <Match exactly pattern="/" component={Stories} />
+            <Match pattern="/about" component={About} />
+            <Match pattern="/new" component={New} />
+            <Match pattern="/popular" component={Popular} />
+            <Match pattern="/submit" component={Submit} />
+          </div>
         </div>
       </div>
     </BrowserRouter>
   );
+}
+
+function illustrationOfTheDay() {
+  return !(new Date().getDay() % 2) ? illustration1 : illustration2;
 }
