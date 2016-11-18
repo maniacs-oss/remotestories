f2bbad450ad530ede5677e002238c39d06f5a305
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-11-18 10:40:26 +0100

    Guard against double submit bug

--- web/src/lib/forming/form.js
+++ web/src/lib/forming/form.js
@@ -3,20 +3,42 @@
 import React from 'react';
 import { elementValues, focusFirstElement } from './utils';
 
+type OnSubmit = (values: Object) => any;
+
 type Props = {
   children?: any,
+  disabled: boolean,
   focus: boolean,
   name?: string,
-  onSubmit?: (values: Object) => any,
+  onSubmit?: OnSubmit,
+};
+
+type State = {
+  isSubmitting: boolean,
+};
+
+const styles = {
+  fieldset: {
+    border: 'none',
+    margin: 0,
+    padding: 0,
+  },
+  submitButton: {
+    display: 'none',
+  },
 };
 
 export default class Form extends React.Component {
   props: Props;
 
+  state: State = { isSubmitting: false };
+
   el: HTMLFormElement;
+
   submitButton: HTMLButtonElement;
 
   static defaultProps = {
+    disabled: false,
     focus: false,
   };
 
@@ -31,31 +53,46 @@ export default class Form extends React.Component {
 
   reset() {
     this.el.reset();
+    this.setState({ isSubmitting: false });
   }
 
   render() {
-    const { children, focus: _, onSubmit: __, ...formProps } = this.props;
+    const { children, disabled, focus: _, onSubmit: __, ...formProps } = this.props;
+    const { isSubmitting } = this.state;
 
     return (
       <form {...formProps} onSubmit={this.handleSubmit} ref={el => this.el = el}>
-        {children}
+        <fieldset disabled={disabled || isSubmitting} style={styles.fieldset}>
+          {children}
 
-        <button
-          style={{ display: 'none' }}
-          ref={button => this.submitButton = button} />
+          <button
+            style={styles.submitButton}
+            ref={submitButton => this.submitButton = submitButton} />
+        </fieldset>
       </form>
     );
   }
 
   handleSubmit = (event: SyntheticEvent) => {
     const { name, onSubmit } = this.props;
-    if (!onSubmit) return;
+
+    if (!onSubmit) {
+      return;
+    }
 
     event.preventDefault();
 
-    const values = elementValues(this.el);
-    const wrappedValues = name ? { [`${ name }`]: values } : values;
+    const { isSubmitting } = this.state;
 
-    onSubmit(wrappedValues);
+    if (isSubmitting) {
+      return;
+    }
+
+    this.setState({ isSubmitting: true }, () => {
+      const values = elementValues(this.el);
+      const wrappedValues = name ? { [`${ name }`]: values } : values;
+
+      onSubmit(wrappedValues);
+    });
   };
 }
