4c0392b45412e6d80976bf0e16f3acdc81b676d8
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-11-26 14:23:15 +0100

    Render a success response on root

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :stories, only: %i(index show create)
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'welcome#index'
+  resources :stories, only: %i(index show create)
 end
