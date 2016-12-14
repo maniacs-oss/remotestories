3811b17beb42ffb3808cdd9bc2f7cbca8eeeb94b
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-12-14 18:22:22 +0100

    Allow non naked domain to consume the api

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost', 'remotestories.com']
+    origins ['localhost', 'remotestories.com', 'www.remotestories.com']
 
     resource '*',
              headers: :any,
