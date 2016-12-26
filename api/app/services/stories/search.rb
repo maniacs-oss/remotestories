20f96e95246427f63dd37a5d855d0906b03f981e
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-12-26 12:35:05 +0100

    Show more stories per page

--- api/app/services/stories/search.rb
+++ api/app/services/stories/search.rb
@@ -7,7 +7,7 @@ class Stories::Search
     value == 'popular' ? scope.popular : scope.by_date
   end
 
-  per_page 2
+  per_page 10
 
   def total_pages
     @total_pages ||= (count / per_page.to_f).ceil
